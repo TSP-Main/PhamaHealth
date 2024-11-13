b37319dc103bf134452603d22971d996ad110674
rohitbhatia1981 <rohitbhatia1@gmail.com>
2024-11-13 21:32:36 +0530

    Added new files

---
+++ private/configuration.php
@@ -0,0 +1,13 @@
+<?php
+define ('PAGELIMIT','20');
+define ("TITLE","Pharma Health");
+define ("SITE_NAME","Pharma Health");
+
+
+define ("DB_HOST","localhost");
+define ("DB_NAME", "pharmahealth-2024");
+define ("DB_USER", "root");
+define ("DB_PASS","");
+
+
+?>
