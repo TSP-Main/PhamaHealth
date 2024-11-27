c298e00131dac8c9f22a00107370cad7612947d6
rohitbhatia1981 <rohitbhatia1@gmail.com>
2024-11-27 11:23:29 +0530

    Update header-questionnaire.php

--- include/header-questionnaire.php
+++ include/header-questionnaire.php
@@ -1,6 +1,6 @@
 <div class="header_2" style="display: flex; align-items: center; justify-content: space-between; padding: 10px;">
-    <img src="<?php echo URL?>images/logo.png" style="margin:auto; max-width:300px" alt="">
-    <button type="button" onClick="fnExitAssessment()" style="background-color:#0D6EFD; color:#FFF; padding: 8px 16px; border: none; border-radius: 4px; cursor: pointer;">
-        Exit
+    <img src="<?php echo URL?>images/logo.png" style="margin:auto; max-width:300px; padding-left:70px" alt="">
+    <button type="button" onClick="fnExitAssessment()" style="background-color:#0D6EFD; color:#FFF; padding: 6px 8px; font-size:13px; border: none; border-radius: 4px; cursor: pointer;">
+        Cancel Consultation
     </button>
 	</div>
