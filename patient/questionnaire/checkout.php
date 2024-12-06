a11f6a407ab48e4e67309d742716594891a95403
rohitbhatia1981 <rohitbhatia1@gmail.com>
2024-12-06 10:36:18 +0530

    6-12

--- patient/questionnaire/checkout.php
+++ patient/questionnaire/checkout.php
@@ -58,7 +58,7 @@ if (count($_SESSION['sessCart'])==0)
 </style>
   <body style="padding-top:0px;"> 
    <div class="header_2">
-       <a href="#"><img src="<?php echo URL?>images/logo.png"></a>
+       <a href="#"><img src="<?php echo URL?>images/logo.png" style="max-width:220px"></a>
    </div>  
    <section class="patient-order-checkout">
        <div class="container">
