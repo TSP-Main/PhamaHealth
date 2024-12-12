7a0bf09a0ef9b3deba51793c29bf790447a1c636
rohitbhatia1981 <rohitbhatia1@gmail.com>
2024-12-12 14:14:23 +0530

    12-12

--- pages/pharmacy.php
+++ pages/pharmacy.php
@@ -238,128 +238,7 @@ patients
 </div>
 </div>
 </section>
-<section class="our_treatments">
-	<div class="container wow fadeInUp" data-wow-delay="0.5s">
-		<h2 class="title_h2 text-center">Our Treatments</h2>
-		<div class="row">
-			<div class="col-sm-3">
-				<div class="treatments_box">
-					<img class="icon_img" src="<?php echo URL?>images/mens_health_icon.png">
-					<h3>Men’s Health</h3>
-					<ul class="list_item_1">
-						<li><a href="<?php echo URL?>treatments/tdetail?c=1"><span>Hair loss</span></a></li>
-						<li><a href="<?php echo URL?>treatments/tdetail?c=10"><span>Erectile dysfunction</span></a></li>
-						<li><a href="<?php echo URL?>treatments/tdetail?c=11"><span>Premature ejaculation</span></a></li>
-					</ul>
-					<div class="mt-auto">
-					<a href="<?php echo URL?>treatments/a-z-conditions" class="btn btn-danger btn-sm float-end">View All</a>
-				</div>
-				</div>
-			</div>
-			<div class="col-sm-3">
-				<div class="treatments_box">
-					<img class="icon_img" src="<?php echo URL?>images/womenshealth-icon.png">
-					<h3>Women’s Health</h3>
-					<ul class="list_item_1">
-						<li><a href="<?php echo URL?>treatments/tdetail?c=28"><span>Contraception</span></a></li>
-						<li><a href="<?php echo URL?>treatments/tdetail?c=26"><span>Period delay</span></a></li>
-						<li><a href="<?php echo URL?>treatments/tdetail?c=62"><span>Cystisis</span></a></li>
-					</ul>
-					<div class="mt-auto">
-						<a href="<?php echo URL?>treatments/a-z-conditions" class="btn btn-danger btn-sm float-end">View All</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-3">
-				<div class="treatments_box">
-					<img class="icon_img" src="<?php echo URL?>images/ChildrensHealth.png">
-					<h3>Children’s Health</h3>
-					<ul class="list_item_1">
-						<li><a href="#"><span>Asthma</span></a></li>
-						<li><a href="#"><span>Acne</span></a></li>
-						<li><a href="#"><span>Dental Problem</span></a></li>
-					</ul>
-					<div class="mt-auto">
-						<a href="<?php echo URL?>treatments/a-z-conditions" class="btn btn-danger btn-sm float-end">View All</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-3">
-				<div class="treatments_box">
-					<img class="icon_img" src="<?php echo URL?>images/ChildrensHealth.png">
-					<h3>General Health</h3>
-					<ul class="list_item_1">
-						<li><a href="<?php echo URL?>treatments/tdetail?c=33"><span>Acid reflux</span></a></li>
-						<li><a href="<?php echo URL?>treatments/tdetail?c=39"><span>Hay fever</span></a></li>
-						<li><a href="<?php echo URL?>treatments/tdetail?c=41"><span>Pain relief</span></a></li>
-					</ul>
-					<div class="mt-auto">
-						<a href="<?php echo URL?>treatments/a-z-conditions" class="btn btn-danger btn-sm float-end">View All</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-3">
-				<div class="treatments_box">
-					<img class="icon_img" src="<?php echo URL?>images/SkinHealth.png">
-					<h3>Skin Health</h3>
-					<ul class="list_item_1">
-						<li><a href="<?php echo URL?>treatments/tdetail?c=49"><span>Eczema</span></a></li>
-						<li><a href="<?php echo URL?>treatments/tdetail?c=51"><span>Acne</span></a></li>
-						<li><a href="<?php echo URL?>treatments/tdetail?c=53"><span>Skin infection</span></a></li>
-					</ul>
-					<div class="mt-auto">
-						<a href="<?php echo URL?>treatments/a-z-conditions" class="btn btn-danger btn-sm float-end">View All</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-3">
-				<div class="treatments_box">
-					<img class="icon_img" src="<?php echo URL?>images/ChronicHealth.png">
-					<h3>Chronic Health</h3>
-					<ul class="list_item_1">
-						<li><a href="#"><span>Asthma/COPD</span></a></li>
-						<li><a href="#"><span>IBS</span></a></li> 
-					</ul>
-					<div class="mt-auto">
-						<a href="<?php echo URL?>treatments/a-z-conditions" class="btn btn-danger btn-sm float-end">View All</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-3">
-				<div class="treatments_box">
-					<img class="icon_img" src="<?php echo URL?>images/ChronicHealth.png">
-					<h3>Sexual Health</h3>
-					<ul class="list_item_1">
-						<li><a href="#"><span>Chlamydia</span></a></li>
-						<li><a href="#"><span>Genital Herpes</span></a></li> 
-						<li><a href="#"><span>Genital Warts</span></a></li> 
-					</ul>
-					<div class="mt-auto">
-						<a href="<?php echo URL?>treatments/a-z-conditions" class="btn btn-danger btn-sm float-end">View All</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-3">
-				<div class="treatments_box">
-					<img class="icon_img" src="<?php echo URL?>images/TravelHealth.png">
-					<h3>Travel Health</h3>
-					<ul class="list_item_1">
-						<li><a href="<?php echo URL?>treatments/tdetail?c=55"><span>Malaria</span></a></li>
-						<li><a href="<?php echo URL?>treatments/tdetail?c=56"><span>Altitude sickness</span></a></li> 
-						<li><a href="<?php echo URL?>treatments/tdetail?c=57"><span>Travelers diarrhea</span></a></li> 
-					</ul>
-					<div class="mt-auto">
-						<a href="<?php echo URL?>treatments/a-z-conditions" class="btn btn-danger btn-sm float-end">View All</a>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-12 text-center mt-3">
-			 <a href="<?php echo URL?>treatments/a-z-treatments" class="me-2 btn btn-danger btn-lg d-inline-flex align-items-center ps-5 pe-5">View A-Z Treatments</a>
-			 <a href="<?php echo URL?>treatments/a-z-conditions" class="ms-2 btn btn-danger btn-lg d-inline-flex align-items-center ps-5 pe-5">View A-Z Conditions</a>
-			</div>
-		</div>
-	</div>
-</section>
+<?php include PATH."include/our-treatment.php"; ?>
 <section class="additional_clinical text-center wow fadeInUp" data-wow-delay="0.5s">
 	<div class="container">
 		<h2 class="title_h2">Additional Clinical Services</h2>
@@ -567,5 +446,17 @@ $(function() {
         }
     });
 	
+});
+$('body').on('click', '.treatments_toggle', function(e) { 
+    var treatmentsBox = $(this).closest('.treatments_box');
+    
+    // Close other level boxes
+    $('.treatments_box').find('.level-box').stop().slideUp();
+    
+    // Toggle the current level-box
+    treatmentsBox.find('.level-box').stop().slideToggle();
+
+    // Toggle the 'active' class on the current treatments_box
+    treatmentsBox.toggleClass('active');
 });
 </script>
